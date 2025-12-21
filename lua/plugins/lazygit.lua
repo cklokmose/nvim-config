3eea05406502c77db8d9f8713303559aec1e9aec
Clemens Nylandsted Klokmose <clemens@klokmose.net>
2025-12-21 21:10:48 +0100

    Updated LazyGit to open in enclosing git folder of the buffer

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,21 +1,46 @@
 -- nvim v0.8.0
+
+local function open_lazygit_at_git_root()
+  local file = vim.fn.expand("%:p")
+
+  if file ~= "" then
+    local git_root = vim.fn.systemlist({
+      "git",
+      "-C", vim.fn.fnamemodify(file, ":h"),
+      "rev-parse",
+      "--show-toplevel",
+    })[1]
+
+    if git_root and git_root ~= "" then
+      vim.cmd("tabnew")
+      vim.cmd("lcd " .. git_root)
+      vim.cmd("LazyGit")
+      return
+    end
+  end
+
+  -- fallback: LazyGit’s own default behaviour
+  vim.cmd("LazyGit")
+end
+
 return {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
+  "kdheepak/lazygit.nvim",
+  lazy = true,
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    {
+      "<leader>lg",
+      open_lazygit_at_git_root,
+      desc = "LazyGit (nearest git root or default)",
     },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+  },
 }
