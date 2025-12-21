b143ef7693125d9fba3c1cd44dac4aec6ffe90f8
Clemens Nylandsted Klokmose <clemens@klokmose.net>
2025-12-21 21:20:23 +0100

    Fixed issue with LazyGit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,5 +1,3 @@
--- nvim v0.8.0
-
 local function open_lazygit_at_git_root()
   local file = vim.fn.expand("%:p")
 
@@ -12,14 +10,12 @@ local function open_lazygit_at_git_root()
     })[1]
 
     if git_root and git_root ~= "" then
-      vim.cmd("tabnew")
       vim.cmd("lcd " .. git_root)
       vim.cmd("LazyGit")
       return
     end
   end
 
-  -- fallback: LazyGit’s own default behaviour
   vim.cmd("LazyGit")
 end
 
