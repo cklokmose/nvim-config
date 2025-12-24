85b87c8334e9fe0ec7ac5f10d5d874996645a29c
Clemens Nylandsted Klokmose <clemens@cs.au.dk>
2025-12-24 08:02:55 +0100

    JavaScript LSP

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,10 +3,23 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      -- keep NvChad’s default keymaps, capabilities, on_attach, etc.
+      -- keep NvChad's default keymaps, capabilities, on_attach, etc.
       require("nvchad.configs.lspconfig").defaults()
 
-      -- new Neovim 0.11+ API
+      -- Configure TypeScript/JavaScript language server
+      vim.lsp.config("ts_ls", {
+        init_options = {
+          hostInfo = "neovim",
+          preferences = {
+            includeInlayParameterNameHints = "all",
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+          },
+        },
+      })
+      vim.lsp.enable("ts_ls")
+
+      -- LaTeX language server
       vim.lsp.config("texlab", {})
       vim.lsp.enable("texlab")
     end,
