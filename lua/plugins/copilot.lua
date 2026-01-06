45aed7fbc7b06aea7aaf55a32e607c3d7e33047e
Clemens Nylandsted Klokmose <clemens@cs.au.dk>
2026-01-06 09:43:26 +0100

    Disabled copilot per default

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,23 @@ return {
   {
     "github/copilot.vim",
     lazy = false,
-  }
+    init = function()
+      -- Disable Copilot globally by default
+      vim.g.copilot_enabled = false
+    end,
+    keys = {
+      {
+        "<leader>ct",
+        function()
+          if vim.fn['copilot#Enabled']() == 1 then
+            vim.cmd('Copilot disable')
+          else
+            vim.cmd('Copilot enable')
+          end
+        end,
+        desc = "Toggle Copilot",
+      },
+    },
+  },
 }
 
