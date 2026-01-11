4f9ea9f8a8cc2a200bb200832f7427e2c8b68f2c
Clemens Nylandsted Klokmose <clemens@klokmose.net>
2026-01-11 15:47:16 +0100

    image.nvim

---
+++ lua/plugins/image.lua
@@ -0,0 +1,24 @@
+return {
+  "3rd/image.nvim",
+  ft = { "markdown", "vimwiki" },  -- Load when opening markdown files
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    backend = "kitty",
+    processor = "magick_cli",
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = false,
+        filetypes = { "markdown", "vimwiki" },
+      },
+    },
+    max_width = nil,
+    max_height = nil,
+    max_width_window_percentage = nil,
+    max_height_window_percentage = 50,
+  },
+}
