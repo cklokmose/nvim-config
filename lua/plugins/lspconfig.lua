3b1dee8f78e1bd0dfe2523da9a54817d87f18b51
Clemens Nylandsted Klokmose <clemens@klokmose.net>
2026-01-18 21:26:30 +0100

    Language server for lua

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,15 @@
 -- ~/.config/nvim/lua/custom/plugins/lspconfig.lua
 return {
   {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = { "lua_ls", "ts_ls", "texlab" },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
+    dependencies = { "mason-org/mason-lspconfig.nvim" },
     config = function()
       -- keep NvChad's default keymaps, capabilities, on_attach, etc.
       require("nvchad.configs.lspconfig").defaults()
@@ -22,6 +30,21 @@ return {
       -- LaTeX language server
       vim.lsp.config("texlab", {})
       vim.lsp.enable("texlab")
+
+      -- Lua language server (configured for Neovim)
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      })
+      vim.lsp.enable("lua_ls")
     end,
   },
 }
